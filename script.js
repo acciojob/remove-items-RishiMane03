e517a678dc22da2c32f2ccbf23e9788127071646
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-29 14:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const deleteBtn = document.getElementById("deleteBtn");
+const dropDown = document.getElementById("colorSelect");
+
+let selectedValue;
+
+deleteBtn.addEventListener('click', ()=>{
+    selectedValue =  dropDown.value
+    colorFinder(selectedValue);
+})
+
+function colorFinder(value){
+
+    for (let index = 0; index < dropDown.length; index++) {
+        // console.log(dropDown[index].value);
+        var color = dropDown[index].value;
+    
+        if(color == value){
+            dropDown[index].remove(); //option delete
+        }
+    
+    }
+}
